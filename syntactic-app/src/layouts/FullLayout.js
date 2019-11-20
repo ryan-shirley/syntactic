d587ac188be47c2254e1f99fd1d7ff2c64fca51a
Ryan Shirley <ryanjshirley@gmail.com>
2019-11-20 19:52:04 +0000

    Update FullLayout.js

--- syntactic-app/src/layouts/FullLayout.js
+++ syntactic-app/src/layouts/FullLayout.js
@@ -3,11 +3,7 @@ import { Route } from 'react-router-dom';
   
 const FullLayout = ({component, path}) => {  
     return (  
-        <div>
-            This is the full layout.
-
-            <Route path={path} component={component}/>
-        </div>
+        <Route path={path} component={component}/>
     )  
 } 
   
