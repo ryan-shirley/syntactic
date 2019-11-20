fc81e6dfc82d238a1c67e0459a53e6f621c60609
Ryan Shirley <ryanjshirley@gmail.com>
2019-11-20 19:51:59 +0000

    Form class with all form components

---
+++ syntactic-app/src/components/Form.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class TextArea extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            value: this.props.value,
+            label: this.props.label
+        }
+
+    }
+
+    /**
+     * getDerivedStateFromProps() Update value with new props
+     */
+    static getDerivedStateFromProps(nextProps, prevState) {
+        return {
+            value: nextProps.value,
+        };
+    }
+
+    render() {
+        return (
+            <div className="form-group">
+                <label htmlFor={this.state.label}>{this.state.label}</label>
+                <textarea className="form-control" rows="3" value={this.state.value} onChange={(e) => this.props.handleChange(this.props.field, e)}  ></textarea>
+            </div>
+        )
+    }
+}
+
+export { TextArea }
