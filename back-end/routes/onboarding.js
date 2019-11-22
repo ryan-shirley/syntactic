9ebb40edd4e92e205dcac4edc7c5986abd53a93a
Ryan Shirley <ryanjshirley@gmail.com>
2019-11-22 19:04:22 +0000

    Express analyse first text using Google NL API

--- back-end/routes/onboarding.js
+++ back-end/routes/onboarding.js
@@ -2,17 +2,49 @@ var express = require('express');
 var router = express.Router();
 
 /* POST Writer onboarding complete. */
-router.post('/writer', function (req, res) {
-
+router.post('/writer', async (req, res) => {
+    // Data from request
     const data = req.body
 
     const bio = data.bio
     const texts = data.texts
     const links = data.links
+    
+    const categories = await classifyText(texts[0])
 
-    res.send({ 
-        body: 'Got a POST request'
+    // console.log('Categories');
+    // categories.then(cat => {
+    //     let { name, confidence } = cat
+    //     console.log(`Name: ${name}, Confidence: ${confidence}.`);
+    // })
+
+    res.send({
+        response: 'Woo! 😃 We were able to get the categories from the text you provided.',
+        categories: categories
     })
 })
 
 module.exports = router;
+
+
+
+/**
+ * classifyText() Derive the categories of content type from text
+ */
+async function classifyText(text) {
+    // Imports the Google Cloud client library
+    const language = require('@google-cloud/language');
+
+    // Creates a client
+    const client = new language.LanguageServiceClient();
+
+    // Prepares a document, representing the provided text
+    const document = {
+        content: text,
+        type: 'PLAIN_TEXT',
+    };
+
+    // Classifies text in the document
+    const [classification] = await client.classifyText({document});
+    return classification
+}
