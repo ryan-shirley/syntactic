6acc5a665e7e53346e77ed592874877d9796d6d8
Ryan Shirley <ryanjshirley@gmail.com>
2019-12-07 17:01:19 +0000

    Added Error handling for adding categories

--- back-end/controllers/writer_controller.js
+++ back-end/controllers/writer_controller.js
@@ -5,68 +5,77 @@ const Category = require("../models/categories.model")
 
 // Take text to categories and add to user
 exports.addText = async (req, res) => {
-    const { uid, text } = req.body
+    try {
+        const { uid, text } = req.body
 
-    let sepCats = await GOOGLE_NL_API.classifyText(text)
-    // console.log(sepCats)
+        let sepCats = await GOOGLE_NL_API.classifyText(text)
+        // console.log(sepCats)
 
-    // Loop through category array
-    sepCats.map(async categoryObj => {
-        let { categories, confidence } = categoryObj
+        // Loop through category array
+        sepCats.map(async categoryObj => {
+            let { categories, confidence } = categoryObj
 
-        for (let i = 0; i < categories.length; i++) {
-            let name = categories[i]
+            for (let i = 0; i < categories.length; i++) {
+                let name = categories[i]
 
-            let existStatus = await CategoryController.checkExists(name)
+                let existStatus = await CategoryController.checkExists(name)
 
-            // console.log(`Checking exist status of: ${name}. It is`, existStatus.exists);
+                // console.log(`Checking exist status of: ${name}. It is`, existStatus.exists);
 
-            if (existStatus.exists) {
-                // Category exists
+                if (existStatus.exists) {
+                    // Category exists
 
-                // Must be last in array to add
-                if (i === categories.length - 1) {
-                    // ADD User
-                    await CategoryController.addUser(
-                        name,
-                        "5dd92e71e5cfa00b6e369d52",
-                        confidence
-                    )
-                }
-            } else {
-                // Category DOESNT exist
-
-                // If first in array & only one in array - create WITHOUT parent - WITH User
-                if (i === 0 && categories.length === 1) {
-                    // Create category WITHOUT parent
-                    await CategoryController.createCategory(name, null, {
-                        _user_id: "5dd92e71e5cfa00b6e369d52",
-                        articles_written: 1,
-                        confidence
-                    })
-                } else if (i === categories.length - 1) {
-                    // Create category WITH parent - WITH User
-                    await CategoryController.createCategory(name, categories[i - 1], {
-                        _user_id: "5dd92e71e5cfa00b6e369d52",
-                        articles_written: 1,
-                        confidence
-                    })
+                    // Must be last in array to add
+                    if (i === categories.length - 1) {
+                        // ADD User
+                        await CategoryController.addUser(
+                            name,
+                            "5dd92e71e5cfa00b6e369d52",
+                            confidence
+                        )
+                    }
                 } else {
-                    // Create category WITH parent - WITHOUT user
-                    await CategoryController.createCategory(
-                        name,
-                        categories[i - 1]
-                    )
+                    // Category DOESNT exist
+
+                    // If first in array & only one in array - create WITHOUT parent - WITH User
+                    if (i === 0 && categories.length === 1) {
+                        // Create category WITHOUT parent
+                        await CategoryController.createCategory(name, null, {
+                            _user_id: "5dd92e71e5cfa00b6e369d52",
+                            articles_written: 1,
+                            confidence
+                        })
+                    } else if (i === categories.length - 1) {
+                        // Create category WITH parent - WITH User
+                        await CategoryController.createCategory(
+                            name,
+                            categories[i - 1],
+                            {
+                                _user_id: "5dd92e71e5cfa00b6e369d52",
+                                articles_written: 1,
+                                confidence
+                            }
+                        )
+                    } else {
+                        // Create category WITH parent - WITHOUT user
+                        await CategoryController.createCategory(
+                            name,
+                            categories[i - 1]
+                        )
+                    }
                 }
             }
-        }
-    })
+        })
 
-    res.send({
-        body:
-            "Woo 😀! Your text has been analysed and we have updated our database with these categories.",
-        categories: sepCats
-    })
+        res.send({
+            body:
+                "Woo 😀! Your text has been analysed and we have updated our database with these categories.",
+            categories: sepCats
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).send({ error })
+    }
 }
 
 // Get categories from user
