3ff54bd7967a095e9e675f9b7e6bb3dd8330335d
Ryan Shirley <ryanjshirley@gmail.com>
2019-12-13 19:43:48 +0000

    Remove endpoints not being used

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -2,14 +2,6 @@ var express = require('express');
 var router = express.Router();
 const auth_controller = require('../controllers/auth_controller');
 
-
-/* GET home page. */
-router.get('/', function (req, res) {
-    res.send({ 
-        body: 'Hello world!' 
-    })
-})
-
 /* Sign Up */
 router.post('/signup', auth_controller.signUp)
 
