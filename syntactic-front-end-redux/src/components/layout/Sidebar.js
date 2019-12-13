59e0e16ea203b7a4ca5c9e3037a1b5fc1e2e9a49
Ryan Shirley <ryanjshirley@gmail.com>
2019-12-13 11:52:12 +0000

    Sidebar icons with Font-Awesome

--- syntactic-front-end-redux/src/components/layout/Sidebar.js
+++ syntactic-front-end-redux/src/components/layout/Sidebar.js
@@ -1,32 +1,34 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome , faThumbtack, faBolt, faWallet, faMoneyBill } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
     return (
         <ul className="nav flex-column">
             <li className="nav-item">
                 <Link className="nav-link" to="/dashboard">
-                    Dashboard
+                    <FontAwesomeIcon icon={faHome} className="text-primary mr-2" style={{width:'20px'}} /> Dashboard
                 </Link>
             </li>
             <li className="nav-item">
                 <Link className="nav-link" to="/projects">
-                    Projects
+                    <FontAwesomeIcon icon={faThumbtack} className="text-warning mr-2" style={{width:'20px'}} /> Projects
                 </Link>
             </li>
             <li className="nav-item">
                 <Link className="nav-link" to="/levels">
-                    Levels
+                    <FontAwesomeIcon icon={faBolt} className="text-info mr-2" style={{width:'20px'}} /> Levels
                 </Link>
             </li>
             <li className="nav-item">
                 <Link className="nav-link" to="/billing">
-                    Billing
+                    <FontAwesomeIcon icon={faWallet} className="text-danger mr-2" style={{width:'20px'}} /> Billing
                 </Link>
             </li>
             <li className="nav-item">
                 <Link className="nav-link" to="/earnings">
-                    Earnings
+                    <FontAwesomeIcon icon={faMoneyBill} className="text-success mr-2" style={{width:'20px'}} /> Earnings
                 </Link>
             </li>
         </ul>
