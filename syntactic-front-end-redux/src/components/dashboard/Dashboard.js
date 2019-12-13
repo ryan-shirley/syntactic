36efba145fa4f7a3b29638892746af4cc3309525
Ryan Shirley <ryanjshirley@gmail.com>
2019-12-13 14:12:05 +0000

    Update Dashboard.js

--- syntactic-front-end-redux/src/components/dashboard/Dashboard.js
+++ syntactic-front-end-redux/src/components/dashboard/Dashboard.js
@@ -1,33 +1,18 @@
 import React, { Component } from "react"
-import { connect } from "react-redux"
-import { Redirect } from "react-router-dom"
 
 class Dashboard extends Component {
-    componentDidMount() {
-        // const { auth } = this.props
-        
-        // if (!auth.uid) return <Redirect to="/signin" />
-
-        console.log("Conponent did mount")
-    }
-
     render() {
-        const { auth, categories } = this.props
-        // if (!auth.uid) return <Redirect to="/signin" />
-
         return (
-            <div>
-                This is the dashboard
+            <div className="card">
+                <div className="card-header">
+                    Dashboard
+                </div>
+                <div className="card-body">
+                    Some info here..
+                </div>
             </div>
         )
     }
 }
 
-const mapStateToProps = state => {
-    // console.log(state);
-    return {
-        auth: state.firebase.auth
-    }
-}
-
-export default connect(mapStateToProps)(Dashboard)
+export default Dashboard
