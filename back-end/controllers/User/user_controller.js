1d8d412cd535dbce5fa7e6e54153d7ebbfdad101
Ryan Shirley <ryanjshirley@gmail.com>
2019-12-14 17:26:47 +0000

    Update user_controller.js

--- back-end/controllers/User/user_controller.js
+++ back-end/controllers/User/user_controller.js
@@ -7,10 +7,11 @@ import admin from "../../firebase-service"
 exports.getUser = async (req, res) => {
     const { authToken } = req
 
-    // Get uid for user
+    // Get uid for user from firebase
     const userInfo = await admin.auth().verifyIdToken(authToken)
     const uid = userInfo.uid
 
+    // Get user data from mongo
     User.findOne({ uid }, (err, user) => {
         if (err) return res.status(400).json(err)
 
