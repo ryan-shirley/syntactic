c8ee7e17c3963008d638b15df8a5b46f5eab0e4b
Ryan Shirley <ryanjshirley@gmail.com>
2019-12-19 13:17:09 +0000

    Use sass file

--- syntactic-front-end-redux/src/App.js
+++ syntactic-front-end-redux/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Routes from './Routes'
 import { BrowserRouter } from "react-router-dom"
-import "./App.css"
+import "./App.scss"
 
 function App() {
     return (
