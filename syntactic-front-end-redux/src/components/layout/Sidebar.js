4fdf31ec5a0e390b897e8cbcbf2275cd531a2117
Ryan Shirley <ryanjshirley@gmail.com>
2020-01-06 17:09:54 +0000

    Sidebar different links for roles

--- syntactic-front-end-redux/src/components/layout/Sidebar.js
+++ syntactic-front-end-redux/src/components/layout/Sidebar.js
@@ -1,38 +1,123 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome , faThumbtack, faBolt, faWallet, faMoneyBill } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+    faHome,
+    faThumbtack,
+    faBolt,
+    faWallet,
+    faMoneyBill
+} from "@fortawesome/free-solid-svg-icons"
+import { connect } from "react-redux"
 
-const Sidebar = () => {
-    return (
-        <ul className="nav flex-column sidebar">
-            <li className="nav-item">
-                <NavLink className="nav-link" to="/dashboard">
-                    <FontAwesomeIcon icon={faHome} className="text-primary mr-2" style={{width:'20px'}} /> Dashboard
-                </NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink className="nav-link" to="/projects">
-                    <FontAwesomeIcon icon={faThumbtack} className="text-orange mr-2" style={{width:'20px'}} /> Projects
-                </NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink className="nav-link" to="/levels">
-                    <FontAwesomeIcon icon={faBolt} className="text-info mr-2" style={{width:'20px'}} /> Levels
-                </NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink className="nav-link" to="/billing">
-                    <FontAwesomeIcon icon={faWallet} className="text-danger mr-2" style={{width:'20px'}} /> Billing
-                </NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink className="nav-link" to="/earnings">
-                    <FontAwesomeIcon icon={faMoneyBill} className="text-success mr-2" style={{width:'20px'}} /> Earnings
-                </NavLink>
-            </li>
-        </ul>
-    )
+const Sidebar = props => {
+    const { user } = props
+
+    if (user.role[0].name === "writer") {
+        return (
+            <ul className="nav flex-column sidebar">
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/dashboard">
+                        <FontAwesomeIcon
+                            icon={faHome}
+                            className="text-primary mr-2"
+                            style={{ width: "20px" }}
+                        />{" "}
+                        Dashboard
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/projects">
+                        <FontAwesomeIcon
+                            icon={faThumbtack}
+                            className="text-orange mr-2"
+                            style={{ width: "20px" }}
+                        />{" "}
+                        Projects
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/levels">
+                        <FontAwesomeIcon
+                            icon={faBolt}
+                            className="text-info mr-2"
+                            style={{ width: "20px" }}
+                        />{" "}
+                        Levels
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/billing">
+                        <FontAwesomeIcon
+                            icon={faWallet}
+                            className="text-danger mr-2"
+                            style={{ width: "20px" }}
+                        />{" "}
+                        Billing
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/earnings">
+                        <FontAwesomeIcon
+                            icon={faMoneyBill}
+                            className="text-success mr-2"
+                            style={{ width: "20px" }}
+                        />{" "}
+                        Earnings
+                    </NavLink>
+                </li>
+            </ul>
+        )
+    } else if (user.role[0].name === "content seeker") {
+        return (
+            <ul className="nav flex-column sidebar">
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/dashboard">
+                        <FontAwesomeIcon
+                            icon={faHome}
+                            className="text-primary mr-2"
+                            style={{ width: "20px" }}
+                        />{" "}
+                        Dashboard
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/projects">
+                        <FontAwesomeIcon
+                            icon={faThumbtack}
+                            className="text-orange mr-2"
+                            style={{ width: "20px" }}
+                        />{" "}
+                        Projects
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/billing">
+                        <FontAwesomeIcon
+                            icon={faWallet}
+                            className="text-danger mr-2"
+                            style={{ width: "20px" }}
+                        />{" "}
+                        Billing
+                    </NavLink>
+                </li>
+            </ul>
+        )
+    } else {
+        return (
+            <ul className="nav flex-column sidebar">
+                <li className="nav-item">
+                    <span className="nav-link">This role doesn't exist</span>
+                </li>
+            </ul>
+        )
+    }
 }
 
-export default Sidebar
+const mapStateToProps = state => {
+    return {
+        user: state.auth.user
+    }
+}
+
+export default connect(mapStateToProps)(Sidebar)
