892c6def945a685b1036045411e29b3e620a3dcb
Ryan Shirley <N00161198@student.iadt.ie>
2020-01-07 15:16:23 +0000

    Google NL API uses cleaned text to save on characters

--- back-end/controllers/google_nl_api.controller.js
+++ back-end/controllers/google_nl_api.controller.js
@@ -12,8 +12,9 @@ exports.classifyText = async text => {
     const client = new language.LanguageServiceClient()
 
     // Prepares a document, representing the provided text
+    let cleanedText = text.replace(/(\s\n)/gm, "").replace(/(\n)/gm, " ")
     const document = {
-        content: text,
+        content: cleanedText,
         type: "PLAIN_TEXT"
     }
 
@@ -38,9 +39,11 @@ exports.analyse = async (req, res) => {
         // Creates a client
         const client = new language.LanguageServiceClient()
 
+        let cleanedText = text.replace(/(\s\n)/gm, "").replace(/(\n)/gm, " ")
+
         // Prepares a document, representing the provided text
         const document = {
-            content: text,
+            content: cleanedText,
             type: "PLAIN_TEXT"
         }
 
@@ -48,50 +51,50 @@ exports.analyse = async (req, res) => {
         const [classification] = await client.classifyText({ document })
         categoriesMatched = seperateCategories(classification.categories)
 
-        // let writers = []
-        // for (let i = 0; i < categoriesMatched.length; i++) {
-        //     let { categories, confidence } = categoriesMatched[i]
-        //     // Convert confidence to percentage
-        //     confidence = confidence.toFixed(2) * 100
+        let writers = []
+        for (let i = 0; i < categoriesMatched.length; i++) {
+            let { categories, confidence } = categoriesMatched[i]
+            // Convert confidence to percentage
+            confidence = confidence.toFixed(2) * 100
 
-        //     let l3Exists = await CategoryController.checkExists(categories[2])
-        //     if (l3Exists.exists) {
-        //         const cat = await Category.findOne({
-        //             name: categories[2]
-        //         }).populate("users.user", "first_name last_name")
+            let l3Exists = await CategoryController.checkExists(categories[2])
+            if (l3Exists.exists) {
+                const cat = await Category.findOne({
+                    name: categories[2]
+                }).populate("users.user", "first_name last_name")
 
-        //         writers.push({
-        //             name: categories[2],
-        //             writers: cat.users,
-        //             confidence
-        //         })
-        //     }
-        //     else {
-        //         let l2Exists = await CategoryController.checkExists(categories[1])
-        //         if (l2Exists.exists) {
-        //             const cat = await Category.findOne({
-        //                 name: categories[1]
-        //             }).populate("users.user", "first_name last_name")
+                writers.push({
+                    name: categories[2],
+                    writers: cat.users,
+                    confidence
+                })
+            }
+            else {
+                let l2Exists = await CategoryController.checkExists(categories[1])
+                if (l2Exists.exists) {
+                    const cat = await Category.findOne({
+                        name: categories[1]
+                    }).populate("users.user", "first_name last_name")
     
-        //             writers.push({
-        //                 name: categories[1],
-        //                 writers: cat.users,
-        //                 confidence
-        //             })
-        //         }
-        //         else {
-        //             const cat = await Category.findOne({
-        //                 name: categories[0]
-        //             }).populate("users.user", "first_name last_name")
+                    writers.push({
+                        name: categories[1],
+                        writers: cat.users,
+                        confidence
+                    })
+                }
+                else {
+                    const cat = await Category.findOne({
+                        name: categories[0]
+                    }).populate("users.user", "first_name last_name")
     
-        //             writers.push({
-        //                 name: categories[0],
-        //                 writers: cat.users,
-        //                 confidence
-        //             })
-        //         }
-        //     }
-        // }
+                    writers.push({
+                        name: categories[0],
+                        writers: cat.users,
+                        confidence
+                    })
+                }
+            }
+        }
         
         res.send({
             categoriesMatched
