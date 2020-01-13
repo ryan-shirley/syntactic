10db99e5f7d08796192abb9e35e17d40013a018b
Ryan Shirley <N00161198@student.iadt.ie>
2020-01-13 15:38:38 +0000

    Update firebase-service.js

--- back-end/firebase-service.js
+++ back-end/firebase-service.js
@@ -1,10 +1,10 @@
 // Firebase 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./google-credentials.json"); // SYNTACTIC_YEAR_4_FIREBASE_ADMIN_SDK_AUTH
+// var serviceAccount = require("./syntactic-iadt-year-4-fb-firebase-adminsdk-auth.json"); // SYNTACTIC_YEAR_4_FIREBASE_ADMIN_SDK_AUTH
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.applicationDefault(),
   databaseURL: "https://syntactic-iadt-year-4-fb.firebaseio.com"
 });
 
