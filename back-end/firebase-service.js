0aa83b8ffb56fe1899ac461ef37739cc089f260d
Ryan Shirley <N00161198@student.iadt.ie>
2020-01-13 15:35:14 +0000

    Update firebase-service.js

--- back-end/firebase-service.js
+++ back-end/firebase-service.js
@@ -1,7 +1,7 @@
 // Firebase 
 var admin = require("firebase-admin");
 
-var serviceAccount = process.env.FIREBASE_AUTH_SDK; // SYNTACTIC_YEAR_4_FIREBASE_ADMIN_SDK_AUTH
+var serviceAccount = require("./google-credentials.json"); // SYNTACTIC_YEAR_4_FIREBASE_ADMIN_SDK_AUTH
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
