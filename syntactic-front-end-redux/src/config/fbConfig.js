d24409c16a4cbec0529ecccd8d1ea8f077b684bf
Ryan Shirley <N00161198@student.iadt.ie>
2020-01-13 18:41:30 +0000

    Fix spelling

--- syntactic-front-end-redux/src/config/fbConfig.js
+++ syntactic-front-end-redux/src/config/fbConfig.js
@@ -9,7 +9,7 @@ var config = {
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-    appId: process.env.REAST_APP_FIREBASE_APP_ID
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 }
 firebase.initializeApp(config)
 
