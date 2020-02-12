23a33798b4caa1e3669d51de8bd370e7a057c299
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-12 16:25:50 +0000

    Fix GCP & firebase env issue

--- back-end/config/firebase-service.js
+++ back-end/config/firebase-service.js
@@ -1,13 +1,14 @@
-// Firebase 
-var admin = require("firebase-admin");
+// Firebase
+require("dotenv").config()
+var admin = require("firebase-admin")
 
 // var serviceAccount = require("./google-credentials.json"); // SYNTACTIC_YEAR_4_FIREBASE_ADMIN_SDK_AUTH
 
 var serviceAccount = JSON.parse(process.env.FIREBASE_AUTH_SERVICE_ACCOUNT)
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://syntactic-iadt-year-4-fb.firebaseio.com"
-});
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://syntactic-iadt-year-4-fb.firebaseio.com"
+})
 
 export default admin
