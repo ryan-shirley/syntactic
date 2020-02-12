a3f93fd78dbc6e1fb053a10441f10c43faadb163
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-12 18:19:24 +0000

    Remove old routes and controllers

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -8,13 +8,6 @@ import { checkIfAuthenticated, checkifWriter, checkifContentSeeker } from "../ap
 const writer_controller = require("../controllers/Writer/writer_controller")
 const user_controller = require("../controllers/User/user_controller")
 
-/* 
- * All Users
- */
-router.get("/", checkIfAuthenticated, user_controller.getUser)
-router.put("/update-bio", checkIfAuthenticated, user_controller.updateBio)
-router.put("/finish-onboarding", checkIfAuthenticated, user_controller.finishOnboarding)
-
 /**
  * Writer Routes
  */
@@ -24,11 +17,6 @@ router.get(
     writer_controller.getCategories
 )
 router.post("/writer/add-content",checkIfAuthenticated, writer_controller.addContent)
-
-/**
- * Content Seeker Routes
- */
-router.put("/update-business-description", checkifContentSeeker, user_controller.updateBusinessDescription)
 
 
 module.exports = router
