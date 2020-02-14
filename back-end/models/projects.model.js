9516917325013716cc7e0d5920a032385a4b57a3
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-14 13:03:08 +0000

    Create projects.model.js

---
+++ back-end/models/projects.model.js
@@ -0,0 +1,91 @@
+const mongoose = require("mongoose")
+
+let DeliverableSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    writer_notes: {
+        type: String
+    },
+    content_seeker_notes: {
+        type: String
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ["accepted", "rejected", "pending approval"],
+        default: "pending approval"
+    }
+})
+
+let ProjectsSchema = new mongoose.Schema({
+    writer_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: false,
+        ref: "User"
+    },
+    content_seeker_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: [
+            "draft",
+            "brief not sufficient",
+            "invitation pending",
+            "invitation rejected",
+            "writing",
+            "writing revision",
+            "waiting deliverable approval",
+            "deliverable approved",
+            "deliverable rejected",
+            "completed"
+        ],
+        default: "draft"
+    },
+    brief: {
+        type: {
+            type: String,
+            enum: ["text", "file"]
+        },
+        path: {
+            type: String
+        },
+        text: {
+            type: String
+        },
+        analysis: {
+            type: Array
+        }
+    },
+    resources: {
+        path: {
+            type: String
+        }
+    },
+    deliverables: {
+        type: [DeliverableSchema]
+    },
+    content: {
+        type: String
+    },
+    end_date: {
+        type: Date,
+        required: true
+    }
+})
+
+// Export the model
+module.exports = mongoose.model("Project", ProjectsSchema)
