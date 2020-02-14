ed29e597f9e7df5bcbebff1d6bbb05ce2bd6f577
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-14 16:17:58 +0000

    Update get current user route

--- back-end/api/routes/users.controller.js
+++ back-end/api/routes/users.controller.js
@@ -8,17 +8,8 @@ const UserService = require("../../services/user.service")
  * route('/current').get() Get currently logged in user
  */
 router.route("/current").get(async (req, res) => {
-    const { authToken } = req
-
-    // Call to service layer - Get current user thats logged in
-    const loggedInUser = await UserService.getCurrentUser(authToken).catch(error => {
-        return res.status(400).json({
-            code: 400,
-            message: error.message
-        })
-    })
     // Return new user
-    return res.status(200).json(loggedInUser)
+    return res.status(200).json(req.user)
 })
 
 /**
