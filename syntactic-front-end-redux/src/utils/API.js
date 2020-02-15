4b4a51b7b90b7f5cd806958be418f4baccc59de2
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-15 20:22:23 +0000

    Create API utils

---
+++ syntactic-front-end-redux/src/utils/API.js
@@ -0,0 +1,27 @@
+import store from "../store"
+import axios from "axios"
+const API_URI = process.env.REACT_APP_BACKEND_API
+
+export default {
+    send: (requestType, url, data = null, authRequired = true) => {
+        // Setup request config
+        let config = {}
+        if(authRequired) {
+            const state = store.getState()
+            let token = state.firebase.auth.stsTokenManager.accessToken
+            config.headers = { Authorization: `Bearer ${token}` }
+        }
+
+        // axios request
+        // Return promise so that .then & .catch can be called from outside the class when this method is called
+        return new Promise((resolve, reject) => {
+            axios[requestType](API_URI + url, data, config)
+                .then(response => {
+                    resolve(response.data)
+                })
+                .catch(error => {
+                    reject(error.response.data)
+                })
+        })
+    }
+}
