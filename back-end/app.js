5c5b868628b596e7d77fa5a76c8afd29c4eea985
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-16 12:55:55 +0000

    Remove cookie parser

--- back-end/app.js
+++ back-end/app.js
@@ -4,7 +4,6 @@ require("dotenv").config()
 var createError = require("http-errors")
 var express = require("express")
 var path = require("path")
-var cookieParser = require("cookie-parser")
 var logger = require("morgan")
 var cors = require("cors")
 
@@ -32,7 +31,6 @@ var app = express()
 app.use(logger("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 
 // Allow react app
