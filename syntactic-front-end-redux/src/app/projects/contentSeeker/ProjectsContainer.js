d21d7bfb87f0f0dc42ac244334497977effaa8be
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-16 15:08:00 +0000

    Display all projects with date formatted

--- syntactic-front-end-redux/src/app/projects/contentSeeker/ProjectsContainer.js
+++ syntactic-front-end-redux/src/app/projects/contentSeeker/ProjectsContainer.js
@@ -1,12 +1,41 @@
 // React
 import React, { Component } from "react"
 import { Link } from "react-router-dom"
+import { Table } from "react-bootstrap"
+import Moment from "react-moment"
 
 // Redux
 import { connect } from "react-redux"
 
 class ProjectsContainer extends Component {
     render() {
+        let projects = this.props.projects.projects
+
+        let projectsTable = (
+            <Table striped bordered hover className="mt-3">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Due Date</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {projects && projects.map(project => (
+                        <tr key={project._id}>
+                            <td>{project.title}</td>
+                            <td><Moment
+                                    format="DD/MM/YYYY"
+                                >
+                                    {project.end_date}
+                                </Moment></td>
+                            <td>{project.status}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        )
+
         return (
             <>
                 <h1>Projects list</h1>
@@ -14,6 +43,8 @@ class ProjectsContainer extends Component {
                 <Link to="/projects/create" className="btn btn-primary">
                     New Project
                 </Link>
+
+                {projectsTable}
             </>
         )
     }
