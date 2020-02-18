50771459bbb3f338f958899f309b5a91b8948ada
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-18 15:24:16 +0000

    Display list of writers in table format

--- syntactic-front-end-redux/src/app/projects/create/WriterComponent.jsx
+++ syntactic-front-end-redux/src/app/projects/create/WriterComponent.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react"
-import { Alert, Form, Row, Col, Button, Spinner } from "react-bootstrap"
-import { Redirect } from "react-router-dom"
+import { Alert, Table, Form, Row, Col, Button, Spinner } from "react-bootstrap"
 
 class WriterComponent extends Component {
     constructor(props) {
@@ -16,15 +15,41 @@ class WriterComponent extends Component {
     // }
 
     render() {
-        let { requestProcessing, error } = this.props.projects
-        
+        let { requestProcessing, error, writersList } = this.props.projects
+
+        let { recommended, relevant, others } = writersList
+
         return (
             <>
-                {error && (
-                    <Alert variant="danger">{error.message}</Alert>
-                )}
+                {error && <Alert variant="danger">{error.message}</Alert>}
 
-                <h2>This is the writer component</h2>
+                {requestProcessing ? (
+                    "Loading writers..."
+                ) : (
+                    <Table striped bordered hover className="mt-3">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Articles Written</th>
+                                <th>Categories</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {recommended &&
+                                recommended.writers.map(writer => (
+                                    <tr
+                                        key={writer._id}
+                                    >
+                                        <td>{writer.user.first_name + ' ' + writer.user.last_name}</td>
+                                        <td>
+                                            {writer.articles_written}
+                                        </td>
+                                        <td>{recommended.categories.join(", ")}</td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </Table>
+                )}
             </>
         )
     }
