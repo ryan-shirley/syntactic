6ba17e9bf8df69d1fd3904ea2be636e658183f5e
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-18 22:50:37 +0000

    App layout center main content with some padding

--- syntactic-front-end-redux/src/app/layout/AppLayout.js
+++ syntactic-front-end-redux/src/app/layout/AppLayout.js
@@ -1,8 +1,11 @@
+// React
 import React from "react"
 import { Route } from "react-router-dom"
 
+// Components
 import Navbar from "../components/navbar/Navbar"
 import Sidebar from "../components/sidebar/Sidebar"
+import { Row, Col } from "react-bootstrap"
 
 const AppLayout = ({ component, path, exact }) => {
     let page = exact ? (
@@ -17,7 +20,11 @@ const AppLayout = ({ component, path, exact }) => {
 
             <div id="page-content-wrapper">
                 <Navbar />
-                <main className="container-fluid">{page}</main>
+                <main className="container-fluid">
+                    <Row className="justify-content-md-center mt-5">
+                        <Col md={8} lg={10}>{page}</Col>
+                    </Row>
+                </main>
             </div>
         </div>
     )
