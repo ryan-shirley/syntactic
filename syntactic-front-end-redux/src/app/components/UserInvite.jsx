b771bccd2dd7312073e8645fc8700501970ca5d8
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-18 20:05:27 +0000

    Fix issue with saving writer id into project

--- syntactic-front-end-redux/src/app/components/UserInvite.jsx
+++ syntactic-front-end-redux/src/app/components/UserInvite.jsx
@@ -6,10 +6,10 @@ export default class UserInvite extends Component {
         let { writer, categories } = this.props
 
         return (
-            <tr key={writer._id}>
+            <tr key={writer.user._id}>
                 <td>{writer.user.first_name + " " + writer.user.last_name}</td>
                 <td>{writer.articles_written}</td>
-                <td><Button onClick={() => this.props.onInviteClick(writer._id)}>Invite</Button></td>
+                <td><Button onClick={() => this.props.onInviteClick(writer.user._id)}>Invite</Button></td>
             </tr>
         )
     }
