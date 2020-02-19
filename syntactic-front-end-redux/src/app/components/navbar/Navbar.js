df08f797795e0a0629b09b228191dcffa8260f4c
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-19 22:11:30 +0000

    Clean up main navbar

--- syntactic-front-end-redux/src/app/components/navbar/Navbar.js
+++ syntactic-front-end-redux/src/app/components/navbar/Navbar.js
@@ -1,39 +1,22 @@
 import React from "react"
-import { Link } from "react-router-dom"
 import SignedInLinks from "./SignedInLinks"
-import SignedOutLinks from "./SignedOutLinks"
 import { connect } from "react-redux"
+import { Row, Col } from "react-bootstrap"
+import Moment from "react-moment"
 
 const Navbar = props => {
     const { auth, user } = props
-
-    const links = auth.uid ? (
-        <SignedInLinks user={user} />
-    ) : (
-        <SignedOutLinks />
-    )
+    const links = auth.uid ? <SignedInLinks user={user} /> : null
 
     return (
-        <nav className="navbar navbar-expand-lg shadow-sm">
-            <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div
-                className="collapse navbar-collapse"
-                id="navbarSupportedContent"
-            >
-                <ul className="navbar-nav ml-auto">{links}</ul>
-            </div>
-        </nav>
+        <div className="main-nav">
+            <Row>
+                <Col className="mt-2 text-muted">
+                    <Moment format=" h:mm A DD/MM/YYYY">{new Date()}</Moment>
+                </Col>
+                <Col className="text-right">{links}</Col>
+            </Row>
+        </div>
     )
 }
 
--- syntactic-front-end-redux/src/app/components/navbar/SignedInLinks.js
+++ syntactic-front-end-redux/src/app/components/navbar/SignedInLinks.js
@@ -1,6 +1,9 @@
+// React
 import React from "react"
-import { NavLink } from "react-router-dom"
 import { connect } from "react-redux"
+import { Image, Dropdown } from "react-bootstrap"
+
+// Actions
 import { signOut } from "../../../store/actions/authActions"
 
 /**
@@ -9,40 +12,24 @@ import { signOut } from "../../../store/actions/authActions"
 const SignedInLinks = props => {
     return (
         <>
-            <li className="nav-item">
-                <NavLink to="/" className="nav-link">
-                    Home
-                </NavLink>
-            </li>
-            <li className="nav-item dropdown">
-                <button
-                    className="nav-link media"
-                    id="navbarDropdownMenuLink"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                >
-                    <img
+            <Dropdown alignRight>
+                <Dropdown.Toggle as="button">
+                    <Image
                         src="/img/profile.jpg"
                         className="mr-3 rounded-circle img-fluid"
                         width="40"
                         height="40"
-                        alt="Profile"
-                    />
-                    <div className="media-body">
-                        <h5 className="mt-0 mb-0">{props.user.first_name} {props.user.last_name}</h5>
-                        <span className="role">{props.user.role && props.user.role.map(role => role.name.toUpperCase())}</span>
-                    </div>
-                </button>
-                <div
-                    className="dropdown-menu dropdown-menu-right"
-                    aria-labelledby="navbarDropdownMenuLink"
-                >
-                    <button className="dropdown-item" onClick={props.signOut}>
+                        alt={props.user.first_name + " " + props.user.last_name}
+                    />{" "}
+                    {props.user.first_name} {props.user.last_name}
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item onClick={props.signOut}>
                         Log Out
-                    </button>
-                </div>
-            </li>
+                    </Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
         </>
     )
 }
