9629c397f34166833861b628785a5bd3a302bf0e
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-20 21:07:32 +0000

    Restrict content seeker from accessing text editor

--- syntactic-front-end-redux/src/app/projects/show/ProjectTextEditorComponent.jsx
+++ syntactic-front-end-redux/src/app/projects/show/ProjectTextEditorComponent.jsx
@@ -1,9 +1,36 @@
+// React
 import React, { Component } from "react"
+import { Redirect } from "react-router-dom"
+
+// Redux
+import { connect } from "react-redux"
 
 class ProjectTextEditorComponent extends Component {
     render() {
+        if (this.props.role == "content seeker" && this.props.project._id) {
+            return <Redirect push to={`/projects/${this.props.project._id}`} />
+        }
+
         return <h3>This is the project text editor</h3>
     }
 }
 
-export default ProjectTextEditorComponent
+// Mapping
+const mapStateToProps = state => {
+    return {
+        role: state.auth.user.role[0].name,
+        project: state.projects.singleProject
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        // getProject: id => dispatch(getProject(id)),
+    }
+}
+
+// Export
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ProjectTextEditorComponent)
