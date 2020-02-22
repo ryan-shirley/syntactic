31274ef545da577f0401dadcfe9003f7a1e15986
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-22 12:48:39 +0000

    Build inital level system requirements

---
+++ back-end/services/level.service.js
@@ -0,0 +1,66 @@
+const confidenceExponent = 1.075
+const baseConfidence = 50
+const maxConfidence = 0.8
+
+const articlesExponent = 1.8
+const baseArticles = 1
+
+/**
+ * requiredForLevel() Return required confidence and articles for a level
+ */
+requiredForLevel = level => {
+    // Confidence required
+    let confidence =
+        Math.floor(baseConfidence * Math.pow(confidenceExponent, level)) / 100
+
+    // Set max confidence
+    if (confidence > maxConfidence) {
+        confidence = maxConfidence
+    }
+
+    // Articles required
+    let numArticles = Math.floor(
+        baseArticles * Math.pow(articlesExponent, level)
+    )
+
+    return {
+        confidence,
+        numArticles
+    }
+}
+
+/**
+ * calcLevel() Return level a user is
+ */
+calcLevel = (confidence, numArticles) => {
+    confidence = confidence / numArticles
+
+    for (let level = 1; level < 10; level++) {
+        let details = requiredForLevel(level)
+
+        if (
+            details.confidence < confidence &&
+            details.numArticles < numArticles
+        ) {
+            continue
+        } else {
+            return level
+        }
+    }
+
+    return level
+}
+
+// Output required details for a levels 1 - 10
+for (var level = 1; level < 10; level++) {
+    var details = requiredForLevel(level)
+    console.log(
+        "level " +
+            level +
+            ": " +
+            details.confidence +
+            " confidence and " +
+            details.numArticles +
+            " articles."
+    )
+}
