d6396ddce5ed69ecaead9fe5bbd2fc9b3cc74313
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-24 17:15:44 +0000

    Show deliverables list

--- syntactic-front-end-redux/src/app/projects/show/ProjectOverviewComponent.jsx
+++ syntactic-front-end-redux/src/app/projects/show/ProjectOverviewComponent.jsx
@@ -22,13 +22,33 @@ class ProjectOverviewComponent extends Component {
 
     render() {
         let { project, requestProcessing } = this.props
-        let { brief } = project
+        let { brief, deliverables } = project
 
         if (!project.title || requestProcessing) {
             return <p>Loading...</p>
         } else {
             return (
                 <>
+                    {deliverables && (
+                        <>
+                            <h6 className="text-uppercase">
+                                Deliverables ({deliverables.length})
+                            </h6>
+                            {deliverables.map(item => (
+                                <Card body key={item._id}>
+                                    <span
+                                        className="text-primary"
+                                        // onClick={() => this.downloadFile(brief.path)}
+                                    >
+                                        {item.title}
+                                    </span>
+                                </Card>
+                            ))}
+                            <hr />
+                        </>
+                    )}
+
+
                     <h6 className="text-uppercase">Brief (1)</h6>
                     <Card body>
                         <span
