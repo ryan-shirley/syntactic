9ba8e7f377cbb0ab1b7d4f17b170ca85e20ba710
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-25 13:18:53 +0000

    Add delete request support

--- syntactic-front-end-redux/src/utils/API.js
+++ syntactic-front-end-redux/src/utils/API.js
@@ -128,5 +128,25 @@ export default {
                     reject(error.response.data)
                 })
         })
+    },
+    delete: (url) => {
+        // Setup request config
+        let config = {}
+        const state = store.getState()
+        let token = state.firebase.auth.stsTokenManager.accessToken
+        config.headers = { Authorization: `Bearer ${token}` }
+
+        // axios request
+        // Return promise so that .then & .catch can be called from outside the class when this method is called
+        return new Promise((resolve, reject) => {
+            axios
+                .delete(API_URI + url, config)
+                .then(response => {
+                    resolve(response.data)
+                })
+                .catch(error => {
+                    reject(error.response.data)
+                })
+        })
     }
 }
