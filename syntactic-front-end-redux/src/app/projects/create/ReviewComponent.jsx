aa022378843d403096ca17dd2ebcfa0efe5c701d
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-25 12:49:27 +0000

    Fix review page not loading on click

--- syntactic-front-end-redux/src/app/projects/create/ReviewComponent.jsx
+++ syntactic-front-end-redux/src/app/projects/create/ReviewComponent.jsx
@@ -4,16 +4,20 @@ import React, { Component } from "react"
 // Components
 import { Row, Col } from "react-bootstrap"
 import Error from "../../components/Error"
+import DataLoading from "../../components/DataLoading"
 
 class ReviewComponent extends Component {
     render() {
-        const { error, singleProject } = this.props.projects
+        const { error, singleProject, requestProcessing } = this.props.projects
 
-        return (
+        return requestProcessing || !singleProject.writer_id ? (
+            <DataLoading />
+        ) : (
             <Row className="justify-content-md-center mt-5">
                 <Col sm={4}>
                     {error && <Error error={error} />}
-                    Waiting on response from {singleProject.writer_id}
+                    Waiting on response from{" "}
+                    {singleProject.writer_id.first_name + ' ' + singleProject.writer_id.last_name}
                 </Col>
             </Row>
         )
