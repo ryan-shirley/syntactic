0baf47442ed0eb903ebb2c5af31fd50627dcc13d
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-25 21:19:04 +0000

    Remove console log when sending email

--- back-end/services/email.service.js
+++ back-end/services/email.service.js
@@ -4,19 +4,16 @@
 exports.welcomeEmail = async user => {
     const sgMail = require("@sendgrid/mail")
     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    
-    console.log('Sending Email..');
-    
 
     const msg = {
         to: user.email,
         from: process.env.SENDGRID_FROM_EMAIL,
-        templateId: 'd-df99bd7fb5e749a3a00d11219887bf9c',
+        templateId: "d-df99bd7fb5e749a3a00d11219887bf9c",
         dynamic_template_data: {
             name: user.first_name,
-            role: user.role[0].name,
-        },
-    };
+            role: user.role[0].name
+        }
+    }
 
     await sgMail.send(msg)
 
