e6a241995ea4a1b452685b5ca0bcd56b8d989580
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-26 17:37:26 +0000

    Restructure checkout form for processing button state

--- syntactic-front-end-redux/src/app/billing/CheckoutForm.jsx
+++ syntactic-front-end-redux/src/app/billing/CheckoutForm.jsx
@@ -2,33 +2,47 @@
 import React from "react"
 
 // Stripe
-import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js"
+import { ElementsConsumer, CardElement } from "@stripe/react-stripe-js"
 
 // API Utils
 import API from "../../utils/API"
 
 // Components
 import CardSection from "./CardSection"
-import  { Redirect } from "react-router-dom"
+import { Redirect } from "react-router-dom"
+import { Spinner, Button, Badge } from "react-bootstrap"
 
-export default function CheckoutForm(props) {
-    const stripe = useStripe()
-    const elements = useElements()
+class CheckoutForm extends React.Component {
+    constructor(props) {
+        super(props)
 
-    const handleSubmit = async event => {
+        this.state = {
+            isProcessing: false,
+            error: ""
+        }
+    }
+
+    /**
+     * handleSubmit() Handle payment submission
+     */
+    handleSubmit = async event => {
         // We don't want to let default form submission happen here,
         // which would refresh the page.
         event.preventDefault()
 
+        this.setState({ isProcessing: true })
+
+        const { stripe, elements } = this.props
+
         if (!stripe || !elements) {
             // Stripe.js has not yet loaded.
-            // Make sure to disable form submission until Stripe.js has loaded.
+            // Make  sure to disable form submission until Stripe.js has loaded.
             return
         }
 
         // Get Client Secret
         let paymentIntent = await API.get(
-            `/payments/${props.payment._id}/intent`
+            `/payments/${this.props.payment._id}/intent`
         )
 
         const result = await stripe.confirmCardPayment(
@@ -37,7 +51,7 @@ export default function CheckoutForm(props) {
                 payment_method: {
                     card: elements.getElement(CardElement),
                     billing_details: {
-                        name: props.name
+                        name: this.props.name
                     }
                 }
             }
@@ -46,29 +60,67 @@ export default function CheckoutForm(props) {
         if (result.error) {
             // Show error to your customer (e.g., insufficient funds)
             console.log(result.error.message)
+            this.setState({ isProcessing: false, error: result.error.message })
         } else {
             // The payment has been processed!
             if (result.paymentIntent.status === "succeeded") {
-                // Wait for stripe webhook to notify to update payment
                 setTimeout(() => {
-                    props.history.replace("/billing")
-                }, 500)
+                    this.props.history.replace("/billing")
+                }, 1000)
             }
         }
     }
 
-    // Redirect if payment payes
-    if(props.payment && props.payment.status === 'paid') {
-        return <Redirect to="/billing" />
-    }
+    render() {
+        let { isProcessing, error } = this.state
 
+        if (this.props.payment && this.props.payment.status === "paid") {
+            return <Redirect to="/billing" />
+        }
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <CardSection />
+
+                {error && (
+                    <Badge pill variant="danger">
+                        {error}
+                    </Badge>
+                )}
+
+                <br />
+
+                <Button
+                    type="submit"
+                    disabled={!this.props.stripe || isProcessing}
+                >
+                    {isProcessing ? (
+                        <>
+                            Processing...
+                            <Spinner
+                                as="span"
+                                animation="grow"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                                className="ml-3"
+                            />
+                        </>
+                    ) : (
+                        "Confirm order"
+                    )}
+                </Button>
+            </form>
+        )
+    }
+}
+
+export default function InjectedCheckoutForm(props) {
     return (
-        <form onSubmit={handleSubmit}>
-            <CardSection />
-            <br />
-            <button disabled={!stripe} className="btn btn-primary">
-                Confirm order
-            </button>
-        </form>
+        <ElementsConsumer>
+            {({ stripe, elements }) => (
+                <CheckoutForm stripe={stripe} elements={elements} payment={props.payment} name={props.name} history={props.history} />
+            )}
+        </ElementsConsumer>
     )
 }
