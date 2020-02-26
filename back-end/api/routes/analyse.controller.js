995edf2149c1210f023f1686f240f72430eb91c7
Ryan Shirley <ryanjshirley@gmail.com>
2020-02-26 17:39:22 +0000

    Remove redundand brief analysis route

--- back-end/api/routes/analyse.controller.js
+++ back-end/api/routes/analyse.controller.js
@@ -7,30 +7,7 @@ const UserService = require("../../services/user.service")
 const LevelService = require("../../services/level.service")
 
 // Middlewares
-import { checkifWriter, checkifContentSeeker } from "../middlewares/auth-middleware"
-
-/**
- * route('/brief').post() Analyse brief text
- */
-router.route("/brief").post(checkifContentSeeker, async (req, res) => {
-    const { text } = req.body
-
-    try {
-        // Call to service layer - Business Logic
-        const results = await GoogleNLPService.classifyText(text)
-        const writers = await GoogleNLPService.getWriters(results)
-
-        // Return writers
-        res.status(200).json(writers)
-    } catch (error) {
-        console.error(error)
-
-        res.status(400).json({
-            code: 400,
-            message: error.details
-        })
-    }
-})
+import { checkifWriter } from "../middlewares/auth-middleware"
 
 /**
  * route('/project').post() Analyse project
