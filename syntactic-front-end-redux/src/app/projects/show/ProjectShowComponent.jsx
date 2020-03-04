7f1c1d6e9de8f090f8e3a0a5cc0d4b6240cfb841
Ryan Shirley <ryanjshirley@gmail.com>
2020-03-04 16:43:21 +0000

    Project tabs design updated

--- syntactic-front-end-redux/src/app/projects/show/ProjectShowComponent.jsx
+++ syntactic-front-end-redux/src/app/projects/show/ProjectShowComponent.jsx
@@ -7,15 +7,16 @@ import ProjectHeadingComponent from "./ProjectHeadingComponent"
 
 class ProjectShowComponent extends Component {
     render() {
-        const { project, user, match } = this.props
+        const { project, user, match, location } = this.props
         let role = user.role[0].name
         let url = match.url
+        let pathname = location.pathname
         return (
             <>
                 <ProjectHeadingComponent project={project} role={role} />
                 <Row className="mb-5">
                     <Col>
-                        <ProjectTabsComponent role={role} path={url} project={project} />
+                        <ProjectTabsComponent role={role} path={url} pathname={pathname} project={project} />
                     </Col>
                 </Row>
             </>
--- syntactic-front-end-redux/src/app/projects/show/ProjectTabsComponent.jsx
+++ syntactic-front-end-redux/src/app/projects/show/ProjectTabsComponent.jsx
@@ -1,19 +1,20 @@
 // React
 import React, { Component } from "react"
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 // Components
 import { ButtonGroup, Button } from "react-bootstrap"
 
 class ProjectTabsComponent extends Component {
     render() {
-        const { role, path, project } = this.props
+        const { role, path, project, pathname } = this.props
+        
         return (
-            <ButtonGroup aria-label="Basic example" className="border-bottom">
-                <Button variant="secondary" as={Link} to={`${path}/overview`} >Overview</Button>
-                <Button variant="secondary" as={Link} to={`${path}/chat`}>Chat</Button>
-                {role === 'writer' && <Button variant="secondary" as={Link} to={`${path}/editor`}>Text Editor</Button>}
-                {role === 'content seeker' && project.deliverables && project.deliverables.some(deliverable => deliverable.status === 'accepted') && <Button variant="secondary" as={Link} to={`${path}/finish`}>Finish</Button>}
+            <ButtonGroup aria-label="Basic example" className="project-tabs">
+                <Button variant="link" as={NavLink} to={`${path}/overview`} className={!pathname.includes('chat') && !pathname.includes('editor') && !pathname.includes('finish') && 'active'}>Overview</Button>
+                <Button variant="link" as={NavLink} to={`${path}/chat`}>Chat</Button>
+                {role === 'writer' && <Button variant="link" as={NavLink} to={`${path}/editor`}>Text Editor</Button>}
+                {role === 'content seeker' && project.deliverables && project.deliverables.some(deliverable => deliverable.status === 'accepted') && <Button variant="link" as={NavLink} to={`${path}/finish`}>Finish</Button>}
             </ButtonGroup>
         )
     }
