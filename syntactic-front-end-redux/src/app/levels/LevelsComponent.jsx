5fc51a9b2018910db76434873de2ba8114b179f2
Ryan Shirley <ryanjshirley@gmail.com>
2020-03-04 22:46:23 +0000

    Levels ui updated to card list

--- syntactic-front-end-redux/src/app/levels/LevelsComponent.jsx
+++ syntactic-front-end-redux/src/app/levels/LevelsComponent.jsx
@@ -2,13 +2,37 @@
 import React, { Component } from "react"
 
 // Components
-import { Card } from "react-bootstrap"
+import { Card, Badge } from "react-bootstrap"
 
 class LevelsComponent extends Component {
     render() {
-        let levels = this.props.levels.map(level => (
-            <Card body className="mb-1" key={level.category}>{level.category + ' -  lv ' + level.level}</Card>
-        ))
+        let levels = (
+            <ul
+                className={
+                    "card-list " +
+                    (this.props.levels.length === 1
+                        ? "single"
+                        : this.props.levels.length === 2
+                        ? "double"
+                        : "multi")
+                }
+            >
+                {" "}
+                {this.props.levels.map(level => (
+                    <li className="item" key={level.category}>
+                        <Card body>
+                            {level.category}{" "}
+                            <Badge
+                                variant={level.level < 2 ? "warning" : level.level <= 4 ? "info" : "success"}
+                                className="float-right badge-md text-uppercase"
+                            >
+                                lv {level.level}
+                            </Badge>
+                        </Card>
+                    </li>
+                ))}
+            </ul>
+        )
 
         return levels
     }
