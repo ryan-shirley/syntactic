29e6113f28613a649c0f3938233b1e9e25c1b468
Ryan Shirley <N00161198@student.iadt.ie>
2020-03-10 13:50:11 +0000

    Update new relic logging level

--- syntactic-front-end-redux/newrelic.js
+++ syntactic-front-end-redux/newrelic.js
@@ -20,7 +20,7 @@ exports.config = {
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level: 'info'
+    level: 'error'
   },
   /**
    * When true, all request headers except for those listed in attributes.exclude
