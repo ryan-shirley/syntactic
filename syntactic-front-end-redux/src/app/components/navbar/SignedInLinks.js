fce5d32ab481184fdd46cf24366488081052eb7b
Ryan Shirley <ryanjshirley@gmail.com>
2020-03-11 18:06:43 +0000

    Dynamic profile picture in navigation

--- syntactic-front-end-redux/src/app/components/navbar/SignedInLinks.js
+++ syntactic-front-end-redux/src/app/components/navbar/SignedInLinks.js
@@ -2,6 +2,7 @@
 import React from "react"
 import { connect } from "react-redux"
 import { Image, Dropdown } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 // Actions
 import { signOut } from "../../../store/actions/authActions"
@@ -11,26 +12,27 @@ import { signOut } from "../../../store/actions/authActions"
  */
 const SignedInLinks = props => {
     return (
-        <>
-            <Dropdown alignRight>
-                <Dropdown.Toggle as="button">
-                    <Image
-                        src="/img/profile.jpg"
-                        className="mr-3 rounded-circle img-fluid"
-                        width="40"
-                        height="40"
-                        alt={props.user.first_name + " " + props.user.last_name}
-                    />{" "}
-                    {props.user.first_name} {props.user.last_name}
-                </Dropdown.Toggle>
+        <Dropdown alignRight>
+            <Dropdown.Toggle as="button">
+                <Image
+                    src={props.user.profile_picture || "/img/default-profile.jpg"}
+                    className="mr-3 rounded-circle img-fluid"
+                    width="40"
+                    height="40"
+                    alt={props.user.first_name + " " + props.user.last_name}
+                />{" "}
+                {props.user.first_name} {props.user.last_name}
+            </Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                    <Dropdown.Item onClick={props.signOut}>
-                        Log Out
-                    </Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-        </>
+            <Dropdown.Menu>
+                <Dropdown.Item as={Link} to="/profile">
+                    Edit Profile
+                </Dropdown.Item>
+                <Dropdown.Item onClick={props.signOut}>
+                    Log Out
+                </Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
     )
 }
 
