0b317a2042b80ec296f22eed469061e1dd97ac4b
Ryan Shirley <ryanjshirley@gmail.com>
2020-03-14 11:50:34 +0000

    Update test name

--- back-end/api/tests/stripe.test.js
+++ back-end/api/tests/stripe.test.js
@@ -1,7 +1,7 @@
 const app = require("../../app") // Link to your server
 const request = require("supertest")(app)
 
-it("Ensure stripe webhook return status 400", async done => {
+it("Stripe webhook fail", async done => {
     const response = await request.post("/webhook/stripe/payment")
 
     expect(response.status).toBe(400)
