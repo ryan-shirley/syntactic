e9dd48fc051f7fec84b6475a04feebbab25e19b8
Ryan Shirley <ryanjshirley@gmail.com>
2020-03-14 11:58:17 +0000

    Rmove unnecessary request in role middleware

--- back-end/api/middlewares/auth-middleware.js
+++ back-end/api/middlewares/auth-middleware.js
@@ -1,5 +1,4 @@
 import admin from "../../config/firebase-service"
-import User from "../../models/user.model"
 
 // Services
 const UserService = require("../../services/user.service")
@@ -41,68 +40,29 @@ export const checkIfAuthenticated = (req, res, next) => {
 }
 
 export const checkifWriter = (req, res, next) => {
-    getAuthToken(req, res, async () => {
-        try {
-            const { authToken } = req
-            const userInfo = await admin.auth().verifyIdToken(authToken)
-            const uid = userInfo.uid
+    const role = req.user.role[0].name
 
-            // Get user data from mongo
-            User.findOne({ uid }, (err, user) => {
-                if (err)
-                    return res.status(400).json({
-                        code: 400,
-                        message: error.message
-                    })
-
-                if (user.role[0].name === "writer") {
-                    return next()
-                } else {
-                    return res.status(401).send({
-                        code: 401,
-                        message:
-                            "You are not authorized to make this request. You do not have the correct role."
-                    })
-                }
-            })
-        } catch (e) {
-            return res.status(401).json({
-                code: 401,
-                message: "You are not authorized to make this request"
-            })
-        }
-    })
+    if (role === "writer") {
+        return next()
+    } else {
+        return res.status(401).send({
+            code: 401,
+            message:
+                "You are not authorized to make this request. You do not have the correct role."
+        })
+    }
 }
 
 export const checkifContentSeeker = (req, res, next) => {
-    getAuthToken(req, res, async () => {
-        try {
-            const { authToken } = req
-            const userInfo = await admin.auth().verifyIdToken(authToken)
-            const uid = userInfo.uid
+    const role = req.user.role[0].name
 
-            // console.log("- - User is authenticated")
-
-            // Get user data from mongo
-            User.findOne({ uid }, (err, user) => {
-                if (err) return res.status(400).json(err)
-
-                if (user.role[0].name === "content seeker") {
-                    return next()
-                } else {
-                    return res.status(401).send({
-                        code: 401,
-                        message:
-                            "You are not authorized to make this request. You do not have the correct role."
-                    })
-                }
-            })
-        } catch (e) {
-            // console.log("– - User is not authenticated")
-            return res.status(401).json({
-                code: 401,
-                message: "You are not authorized to make this request"
-            })
-        }
-    })
+    if (role === "content seeker") {
+        return next()
+    } else {
+        return res.status(401).send({
+            code: 401,
+            message:
+                "You are not authorized to make this request. You do not have the correct role."
+        })
+    }
 }
